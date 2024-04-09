e2b5805c8b7bdec962ce1ca9c0e7c737c5c28428
nicx01 <nico.ojer@gmail.com>
2024-04-09 20:52:14 +0200

    0.9.1. Generar métodos get y set para la claseCírculo.

--- utilidades/Circulo.java
+++ utilidades/Circulo.java
@@ -1,40 +1,41 @@
 package utilidades;
 
 public class Circulo {
-	private double rad;
+    private double rad;
 
-	public Circulo(double radio)
-	    	{
-	        this.rad = radio;
-	}
+    public Circulo(double radio) {
+        this.rad = radio;
+    }
 
-	    	public void imprimir()
-	    	{
-	        String color = "rojo";
-	        System.out.println("Di�metro: " + 2*rad);
-	        System.out.println("Color: " + color);
-	        double area = 2 * 3.1416 * rad * rad;
-	        System.out.println(area);
-	}
+    public double getRad() {
+        return rad;
+    }
 
-	public boolean esIgual (Circulo otro, boolean conDecimales)
-	{
-	        double radio1 = this.rad;
-	        double radio2 = otro.getRad();
-	        if (conDecimales)
-	        {
-	            if (radio1 == radio2) 
-	              return true;
-	            else
-	              return false;
-	        }
-	        else
-	        {
-	            if (Math.abs(radio1-radio2)<1)
-	                return true;
-	            else
-	                return false;
-	        }
-	    }
-	}
+    public void setRad(double radio) {
+        this.rad = radio;
+    }
 
+    public void imprimir() {
+        String color = "rojo";
+        System.out.println("Diámetro: " + 2 * rad);
+        System.out.println("Color: " + color);
+        double area = 2 * 3.1416 * rad * rad;
+        System.out.println(area);
+    }
+
+    public boolean esIgual(Circulo otro, boolean conDecimales) {
+        double radio1 = this.rad;
+        double radio2 = otro.getRad();
+        if (conDecimales) {
+            if (radio1 == radio2)
+                return true;
+            else
+                return false;
+        } else {
+            if (Math.abs(radio1 - radio2) < 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
